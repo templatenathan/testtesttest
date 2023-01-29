9c1bd71698d1e481bdf59e09103497a8b5fbcf19
nathanvass <nathanvass@ibm.com>
2023-01-29 17:02:57 -0500

    feat: test

--- test/template.test.js
+++ test/template.test.js
@@ -2,6 +2,6 @@ const placeholder = require("../src/template");
 
 describe("Placeholder test", () => {
   it("placeholder", () => {
-    expect(placeholder).toBe("placeholder");
+    expect(placeholder).toBe("placeholderd");
   });
 });
